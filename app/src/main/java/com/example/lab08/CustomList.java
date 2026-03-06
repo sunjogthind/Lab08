799be6011c3110c0a963deb2a008a402b362b583
Rana Sunjog Singh Thind <71564731+sunjogthind@users.noreply.github.com>
2026-03-05 20:07:13 -0700

    pass test

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -17,4 +17,12 @@ public class CustomList {
     public boolean hasCity(City city) {
         return cities.contains(city);
     }
+
+    public void deleteCity(City city) {
+        if (cities.contains(city)) {
+            cities.remove(city);
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
 }
